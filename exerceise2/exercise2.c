7924a576effe6c582d7d91ab6eb84df77f8ea24c
Nick Robertson <32169500+NickRobertsonPY@users.noreply.github.com>
2018-01-22 20:30:23 +0000

    Create exercise2.c

---
+++ exerceise2/exercise2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(void)
+
+{
+    int x;
+    char y;
+    _Bool z;
+
+    printf("Size of int: %2ld bytes\n", sizeof(x));
+    printf("Size of char: %2ld bytes\n", sizeof(y));
+    printf("Size of bool: %2ld bytes\n", sizeof(z));
+
+
+}
+
