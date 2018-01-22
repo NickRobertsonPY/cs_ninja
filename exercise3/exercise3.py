19984c14ea724783c1c0e10e10350ff7dd433368
Nick Robertson <32169500+NickRobertsonPY@users.noreply.github.com>
2018-01-22 20:57:18 +0000

    Create exercise3.py

---
+++ exercise3/exercise3.py
@@ -0,0 +1,14 @@
+
+def main():
+
+    x = 42
+    y = "James Bond"
+    z = True
+
+    s = ("{} {} {}".format(x, y, z))
+    print(s)
+    print(type(s))
+
+
+if __name__ == '__main__':
+    main()
