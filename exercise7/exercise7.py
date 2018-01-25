d9de1a1d171ae5a44ae090c1d1aeb1140ba3cc7b
Nick Robertson <32169500+NickRobertsonPY@users.noreply.github.com>
2018-01-25 02:05:07 +0000

    Create exercise7.py

---
+++ exercise7/exercise7.py
@@ -0,0 +1,13 @@
+
+def main():
+    x = 5 == 5
+    y = 5 >= 2
+    z = 5 <= 10
+    a = 10 > 5
+    b = 5 < 10
+    c = 5 != 4
+
+    print(x, y, z, a, b, c)
+
+if __name__ == '__main__':
+    main()
