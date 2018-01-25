060f08b85e87ea8dd513dba25d46c82815445fab
Nick Robertson <32169500+NickRobertsonPY@users.noreply.github.com>
2018-01-25 02:05:26 +0000

    Create exercise7.go

---
+++ exercise7/exercise7.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+	x := (5 == 5)
+	y := (10 <=15)
+	z := (15 >= 10)
+	a := (10 != 5)
+	b := (10 < 15)
+	c := (15 > 10)
+	
+	fmt.Println(x, y, z, a, b, c)
+	
+}
